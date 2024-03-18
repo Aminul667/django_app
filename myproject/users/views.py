918d0b89adaea7e73bcc789171b20ec3ba2e0579
Aminul667 <rahathssn@gmail.com>
2024-03-18 14:37:50 +0600

    added views

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
+
+
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
@@ -11,3 +13,13 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('posts:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
